0f6dd6cc6e8805bc24f971d478875c845f322b1e
joowon5685 <joowon5685@gmail.com>
2018-06-20 01:20:24 +0900

    주요일정(2018-06-20)

--- app/src/main/java/com/example/minsuk/smartplaner/PlanEdit.java
+++ app/src/main/java/com/example/minsuk/smartplaner/PlanEdit.java
@@ -3,6 +3,7 @@ package com.example.minsuk.smartplaner;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
@@ -29,6 +30,9 @@ public class PlanEdit extends Activity {
     Button actionView; //현재 버튼 설정이 없음 연결 할수 없음
     Button cencleView;
 
+    int mYear;
+    int mMonth;
+    int mDay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -38,9 +42,9 @@ public class PlanEdit extends Activity {
         setContentView(R.layout.planedit);
 
         Intent intent = getIntent();
-        int mYear = intent.getIntExtra("mYear", 0);
-        int mMonth = intent.getIntExtra("mMonth", 0);
-        int mDay = intent.getIntExtra("mDay", 0);
+        mYear = intent.getIntExtra("mYear", 0);
+        mMonth = intent.getIntExtra("mMonth", 0);
+        mDay = intent.getIntExtra("mDay", 0);
         String mWeek = intent.getStringExtra("mWeek");
 
         String  str = String.format("%d.%d.%d %s",mYear,mMonth,mDay,mWeek);
@@ -92,22 +96,60 @@ public class PlanEdit extends Activity {
                 else { //일정내용이 존재할때
                     SQLiteDatabase db = helper.getWritableDatabase();
 
-                    String insertSQL = "insert into s_time(" +
+                    String insertSQL = "insert into s_date(" +
+                            "year, " +
+                            "month, " +
+                            "day) " +
+                            "values(" +
+                            mYear + ", " +
+                            mMonth + ", " +
+                            mDay + ") ";
+
+                    db.execSQL(insertSQL);
+
+                    insertSQL = "select _id " +
+                            "from s_date " +
+                            "where year = " + mYear + " " +
+                            "and month = " + mMonth + " " +
+                            "and day = " + mDay + " " +
+                            "order by _id";
+
+                    Cursor cursor = db.rawQuery(insertSQL, null);
+
+                    int s_date_id=0;
+
+                    try {
+                        if (cursor != null && cursor.moveToFirst()) {
+                            s_date_id = cursor.getInt(0);
+                        }
+                    }
+                    finally {
+                        if(cursor != null) {
+                            cursor.close();
+                        }
+                    }
+                    cursor.close();
+
+
+                    insertSQL = "insert into s_time(" +
                             "start_time_hour, " +
                             "start_time_min, " +
                             "end_time_hour, " +
                             "end_time_min, " +
                             "content, " +
-                            "place) " +
+                            "place, " +
+                            "date_id) " +
                             "values(" +
                             starthour + ", " +
                             startmin + ", " +
                             endhour + ", " +
                             endtmin + ", " +
                             "'" + content + "', " +
-                            "'" + place + "') ";
+                            "'" + place + "', " +
+                            s_date_id + ") ";
 
                     db.execSQL(insertSQL);
+
                     db.close();
 
                     Toast toast = Toast.makeText(getApplicationContext(), "일정이 저장 되었습니다.", Toast.LENGTH_SHORT);
@@ -127,9 +169,6 @@ public class PlanEdit extends Activity {
                 finish();
             }
         });
-
-
-
 
     }
 }
